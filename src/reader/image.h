1cd85a4c13b17ad21e5e15ab69aefad1e5333f63
Uskrai <30656271+Uskrai@users.noreply.github.com>
2020-11-30 22:01:11 +0800

    Making image visible changing parent in image to wxScrolledWindow Making image centered horizontally

--- src/reader/image.h
+++ src/reader/image.h
@@ -19,8 +19,9 @@
 #define READER_IMAGE
 
 #include "traverser.h"
+#include "config.h"
 
-#include <wx/window.h>
+#include <wx/scrolwin.h>
 #include <wx/image.h>
 #include <wx/bitmap.h>
 #include <wx/vector.h>
@@ -33,8 +34,8 @@ namespace Reader
 
 enum VectorPos
 {
-    VECTOR_PUSH = -1,
-    VECTOR_BEGIN
+    VECTOR_PUSH = -1,   // used when using vector.push_back(val)
+    VECTOR_BEGIN        // used when using vector.insert(vector.begin() val) 
 };
 
 class Image :
@@ -42,35 +43,52 @@ class Image :
 {
         typedef wxSharedPtr<wxBitmap> BitmapPtr;
         typedef wxSharedPtr<wxImage> ImagePtr;
-        typedef wxVector<BitmapPtr> vectorBitmap;
-        typedef wxVector<ImagePtr> vectorImage;
+        typedef wxVector<BitmapPtr> VectorBitmap;
+        typedef wxVector<ImagePtr> VectorImage;
 
     public:
-        Image( wxWindow* parent );
+        Image( wxScrolledWindow* parent );
+        ~Image();
         void Open( wxString path );
+        VectorBitmap Get(); // return all Loaded Bitmap
+        void Clear(); // clear all value
+        wxScrolledWindow* GetParent() { return this->parent; }
+
+        // return imagePos X and Y as wxPoint
+        wxPoint GetPosition( int index) { return wxPoint(this->imagePosX.at(index), this->imagePosY.at(index) );}
 
     private:
-        wxWindow* parent;
+        wxScrolledWindow* parent;
 
-        bool isLoadBitmap = false;
-        void Load( wxString path );
-        bool LoadAt( int index, VectorPos vectorPos = VECTOR_PUSH );
-        void LoadBitmap( ImagePtr img, VectorPos pos ) { }
+        Config* config = Config::Get();
 
-        bool isPreLoadImage = false;
-        void PreLoadImage();
+        void Load( wxString path ); // load image and
+        bool LoadAt( int index, VectorPos vectorPos = VECTOR_PUSH ); // load image at given index and whether to push or insert
+        void LoadBitmap( ImagePtr img, VectorPos pos = VECTOR_PUSH ); // load bitmap from given image and whether to push or insert
+
+        bool isLoadBitmap = false; // turned on when loading Bitmap immediately in multi-thread
+        bool isPreLoadImage = false; // turned on when preLoading Image in multi-thread
+
+        void RefreshImagePosition(); // recalculate all image position
+        void AddPosition( const BitmapPtr& bmp, wxVector<int>& x, wxVector<int>& y); // calculate position from given bitmap
+        void AddPosition( const BitmapPtr& bmp ); // overrided function of above used to add position to imagePos X and Y
+        int GetCenteredPosition( int width ); // Get Centered Position of given Bitmap
+
 
         wxThread::ExitCode Entry();
+        wxCriticalSection gCS; // useed for locking used variable to make thread-safe
         bool TestDestroy() { return this->GetThread()->TestDestroy(); }
 
         Traverser* files = new Traverser();
         wxString file;
 
-        vectorImage image;
-        int prev, next;
-        vectorBitmap bitmap;
+        VectorImage image;
+        int prev, next, showPrev, showNext, cachePrev,cacheNext, maxWidth = 0;
+        VectorBitmap bitmap;
+        wxVector<int> imagePosY, imagePosX;
+        // wxVector<wxPoint> imagePos;
 };
 
 };
 
-#endif
+#endif 
