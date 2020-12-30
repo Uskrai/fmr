76cc0555196561dc7f99e24b454b0d4f228bfde0
Uskrai <30656271+Uskrai@users.noreply.github.com>
2020-12-30 18:26:04 +0800

    update handler test unit

--- test/handler.cpp
+++ test/handler.cpp
@@ -2,11 +2,45 @@
 #include <iostream>
 #include <gtest/gtest.h>
 #include "handler/std_handler.h"
+#include "handler/default_handler.h"
+#include "handler/wxarchive_handler.h"
 
 namespace fmr
 {
 
 std::wstring test_path = L"test_dir";
+
+void TestSizeHandler( AbstractHandler *handler, const std::wstring &path )
+{
+	std::filesystem::create_directories( path + L"/test1" );
+	std::filesystem::create_directories( path + L"/test2" );
+
+	handler->Open( path );
+
+	SStream stream;
+
+	bool cont = handler->GetFirst( stream, kDirDefault, false );
+
+	size_t i = 0;
+
+	while ( cont )
+	{
+		i++;
+		cont = handler->GetNextStream( stream, false );
+	}
+
+	handler->Traverse();
+	EXPECT_EQ( i, handler->Size() );
+}
+
+void TestSortHandler( DefaultHandler *handler, const std::wstring &path )
+{
+	handler->Open( path );
+	handler->Traverse();
+	EXPECT_EQ( handler->Item(0).GetString(), path + L"/test1" );
+
+	std::filesystem::remove_all( path );
+}
 
 TEST(STDHandlerTest, Size)
 {
@@ -34,6 +68,43 @@ TEST( STDHandlerTest, Sort )
 	EXPECT_EQ( handler->Item(0).GetString(), test_path + "/test1" );
 
 	std::filesystem::remove_all( test_path );
+}
+
+// TEST( wxArchiveTest, Size )
+// {
+// 	WxArchiveHandler *handler = new WxArchiveHandler( "test_zip.zip" );
+// 	SStream stream;
+// 	bool cont = handler->GetFirst( stream, kDirDefault, false );
+// 	size_t i = 0;
+
+// 	while ( cont )
+// 	{
+// 		i++;
+// 		cont = handler->GetNextStream( stream, false );
+// 	}
+
+// 	handler->Traverse();
+// 	EXPECT_EQ( i, handler->Size() );
+// }
+
+// TEST( wxArchiveTest, Sort )
+// {
+// 	WxArchiveHandler *handler = new WxArchiveHandler( "test_zip.zip" );
+// 	handler->Traverse();
+
+// 	EXPECT_EQ( handler->Item(0).GetString(), L"test_zip/" );
+// }
+
+TEST( DefaultHandlerTest, Size )
+{
+	DefaultHandler *handler = new DefaultHandler();
+	TestSizeHandler( handler, test_path );
+}
+
+TEST( DefaultHandlerTest, Sort )
+{
+	DefaultHandler *handler = new DefaultHandler();
+	TestSortHandler( handler, test_path );
 }
 
 } // namespace fmr
