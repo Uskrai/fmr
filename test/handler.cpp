f1913aad9809895e82f35bdd4e35d07723ac1749
Uskrai <30656271+Uskrai@users.noreply.github.com>
2020-12-31 13:16:48 +0800

    add Test unit for write method in STDHandler

--- test/handler.cpp
+++ test/handler.cpp
@@ -8,12 +8,12 @@
 namespace fmr
 {
 
-std::wstring test_path = L"test_dir";
+std::wstring test_path = L"test_dir/";
 
 void TestSizeHandler( AbstractHandler *handler, const std::wstring &path )
 {
-	std::filesystem::create_directories( path + L"/test1" );
-	std::filesystem::create_directories( path + L"/test2" );
+	std::filesystem::create_directories( path + L"test1" );
+	std::filesystem::create_directories( path + L"test2" );
 
 	handler->Open( path );
 
@@ -37,15 +37,15 @@ void TestSortHandler( DefaultHandler *handler, const std::wstring &path )
 {
 	handler->Open( path );
 	handler->Traverse();
-	EXPECT_EQ( handler->Item(0).GetString(), path + L"/test1" );
+	EXPECT_EQ( handler->Item(0).GetString(), path + L"test1" );
 
 	std::filesystem::remove_all( path );
 }
 
 TEST(STDHandlerTest, Size)
 {
-	std::filesystem::create_directories( test_path + L"/test1");
-	std::filesystem::create_directories( test_path + L"/test2");
+	std::filesystem::create_directories( test_path + L"test1");
+	std::filesystem::create_directories( test_path + L"test2");
 	STDHandler *handler = new STDHandler();
 	handler->Open( test_path );
 	SStream stream;
@@ -65,9 +65,46 @@ TEST( STDHandlerTest, Sort )
 {
 	STDHandler *handler = new STDHandler( test_path );
 	handler->Traverse();
-	EXPECT_EQ( handler->Item(0).GetString(), test_path + "/test1" );
+	EXPECT_EQ( handler->Item(0).GetString(), test_path + "test1" );
 
 	std::filesystem::remove_all( test_path );
+}
+
+template<typename T>
+void TEST_WRITE( T *handler, const std::wstring path )
+{
+	SStream stream("Makefile");
+	handler->CommitWrite();
+	handler->MakeDir(L"test1");
+	handler->MakeFile( L"owo", stream );
+	handler->CommitWrite();
+	handler->Reset();
+
+	handler->Open( test_path );
+	handler->Traverse( true );
+
+	EXPECT_EQ( handler->Size(), 2 );
+
+	size_t index = handler->Index( handler->GetName() + L"owo" );
+
+	EXPECT_EQ( handler->Item( index ).GetSize(), stream.GetSize() );
+	EXPECT_EQ( handler->Item( index ).GetString(), std::wstring( path + L"owo" ) );
+
+	handler->RemoveAll();
+	handler->CommitWrite();
+	handler->CommitWrite();
+	handler->Clear();
+	handler->Traverse();
+
+	EXPECT_EQ( handler->Size(), 0 );
+
+	handler->CommitWrite();
+}
+
+TEST( STDHandlerTest, Write )
+{
+	STDHandler *handler = new STDHandler( test_path );
+	TEST_WRITE( handler, test_path );
 }
 
 // TEST( wxArchiveTest, Size )
