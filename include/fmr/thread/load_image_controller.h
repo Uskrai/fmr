db29b39e05d22e7cf1ce217ad39ffc8ea826cb87
Uskrai <30656271+Uskrai@users.noreply.github.com>
2021-01-25 22:23:21 +0800

    Add default Controller for LoadImage

---
+++ include/fmr/thread/load_image_controller.h
@@ -0,0 +1,62 @@
+/*
+ *  Copyright (c) 2021 Uskrai
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#ifndef FMR_THREAD_LOAD_IMAGE_CONTROLLER
+#define FMR_THREAD_LOAD_IMAGE_CONTROLLER
+
+#include "fmr/handler/struct_stream.h"
+#include "fmr/thread/load_image.h"
+#include "fmr/thread/thread.h"
+
+namespace fmr {
+namespace thread {
+
+constexpr int kLoadImageControllerIdDefault = wxID_HIGHEST + 4000;
+
+class LoadImageController : public ThreadController {
+ protected:
+  LoadImage *thread_ = nullptr;
+
+  int thread_id_ = kLoadImageControllerIdDefault;
+  wxEvtHandler *parent_ = nullptr;
+  wxCriticalSection lock_;
+
+ public:
+  LoadImageController(wxEvtHandler *parent,
+                      int thread_id = kLoadImageControllerIdDefault);
+  void Push(SStream *stream);
+  bool Run();
+
+  virtual void SetParent(wxEvtHandler *parent) { parent_ = parent; }
+  virtual wxEvtHandler *GetParent() { return parent_; }
+
+  void DoSetNull(int id);
+  wxThread *GetThread(int id);
+
+  void SetThreadId(int id);
+  int GetThreadId() const { return thread_id_; }
+
+  void Clear();
+
+ private:
+  void OnImageLoaded(LoadImageEvent &event);
+};
+
+}  // namespace thread
+}  // namespace fmr
+
+#endif /* end of include guard: FMR_THREAD_LOAD_IMAGE_CONTROLLER */
---
+++ src/thread/load_image_controller.cpp
@@ -0,0 +1,66 @@
+/*
+ *  Copyright (c) 2021 Uskrai
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#include "fmr/thread/load_image_controller.h"
+
+namespace fmr {
+namespace thread {
+
+LoadImageController::LoadImageController(wxEvtHandler *parent, int id)
+    : ThreadController() {
+  SetParent(parent);
+  SetThreadId(id);
+}
+
+void LoadImageController::SetThreadId(int id) {
+  Unbind(kEventImageLoaded, &LoadImageController::OnImageLoaded, this,
+         GetThreadId());
+  thread_id_ = id;
+  Bind(kEventImageLoaded, &LoadImageController::OnImageLoaded, this,
+       GetThreadId());
+}
+
+void LoadImageController::OnImageLoaded(LoadImageEvent &event) {
+  //
+}
+
+void LoadImageController::Push(SStream *stream) {
+  if (!thread_) Run();
+  thread_->Push(stream);
+}
+
+bool LoadImageController::Run() {
+  auto thread_temp = new LoadImage(this, wxTHREAD_DETACHED, GetThreadId());
+  bool ret = thread_temp->Run() == wxTHREAD_NO_ERROR;
+  thread_ = thread_temp;
+
+  return ret;
+}
+
+void LoadImageController::Clear() { DeleteThread(GetThreadId(), lock_); }
+
+void LoadImageController::DoSetNull(int id) {
+  if (id == GetThreadId()) thread_ = nullptr;
+}
+
+wxThread *LoadImageController::GetThread(int id) {
+  if (id == GetThreadId()) return thread_;
+  return nullptr;
+}
+
+}  // namespace thread
+}  // namespace fmr
