c7bdc559aea3ce4564b159796f214b28b58dabeb
Uskrai <30656271+Uskrai@users.noreply.github.com>
2021-01-28 17:14:55 +0800

    Create reader::Controller

---
+++ src/reader/controller.cpp
@@ -0,0 +1,303 @@
+/*
+ *  Copyright (c) 2021 Uskrai
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#include "fmr/reader/controller.h"
+
+#include "fmr/common/dimension.h"
+#include "fmr/common/event.h"
+
+namespace fmr {
+
+namespace reader {
+
+Controller::Controller() {
+  window_ = new ScrolledImageWindow();
+  loader_ = std::make_unique<bitmap::PageLoader>(this, kLoaderId);
+  loader_->SetImagePerPage(2);
+
+  is_read_from_right_ = true;
+  position_ctrl_ = std::make_unique<bitmap::PositionCtrl>(
+      bitmap::kPositionAlignCenter | bitmap::kPositionVertical);
+
+  rescaler_ = std::make_unique<bitmap::Rescaler>(bitmap::kRescaleNone);
+
+  page_bitmap_ctrl_ = std::make_unique<bitmap::PageBitmapCtrl>(
+      position_ctrl_.get(), rescaler_.get());
+
+  Bind(thread::kEventImageLoaded, &Controller::OnLoadedImage, this, kLoaderId);
+
+  Bind(bitmap::kEventOpenedStreamFound, &Controller::OnOpenedStreamFound, this,
+       kLoaderId);
+}
+
+Controller::Controller(wxWindow *parent, wxWindowID id, const wxPoint &pos,
+                       const wxSize &size, long style, const wxString &name)
+    : Controller() {
+  CreateWindow(parent, id, pos, size, style, name);
+}
+
+bool Controller::CreateWindow(wxWindow *parent, wxWindowID id,
+                              const wxPoint &pos, const wxSize &size,
+                              long style, const wxString &name) {
+  bool ret = window_->Create(parent, id, pos, size, style, name);
+
+  event::Bind(window_, kScrollWinEventAll, &Controller::OnWindowScroll, this);
+  return ret;
+}
+
+bool Controller::Open(const std::string &path) {
+  if (loader_->Open(path)) {
+    window_->Scroll(0, 0);
+    return loader_->Run();
+  }
+
+  return false;
+}
+
+void Controller::SetWindow(ScrolledImageWindow *window) {
+  if (window_) window_->Destroy();
+
+  window_ = window;
+
+  if (window_) {
+  }
+}
+
+void Controller::AdjustBitmap() {
+  bitmap::BitmapPage *page = page_bitmap_ctrl_->GetBitmapPage();
+  if (page) {
+    wxPoint first_shown_pos;
+    const SBitmap *first_shown = nullptr;
+    for (const auto &it : page->GetBitmap()) {
+      if (it.IsShown(window_->GetViewStart(), window_->GetClientSize())) {
+        first_shown_pos = it.GetPosition();
+        first_shown = &it;
+        break;
+      }
+    }
+
+    position_ctrl_->SetMinimumSize(window_->GetClientSize());
+    rescaler_->SetMaximumSize(window_->GetClientSize());
+    page_bitmap_ctrl_->AdjustBitmap();
+
+    wxSize size = page_bitmap_ctrl_->GetSize(page);
+
+    window_->SetVirtualSize(size);
+    position_ctrl_->SetWindowSize(size);
+    page_bitmap_ctrl_->RecalcPosition(page);
+
+    if (window_->GetVirtualSize().GetHeight() >
+            window_->GetClientSize().GetHeight() &&
+        window_->GetVirtualSize().GetWidth() >
+            window_->GetClientSize().GetWidth()) {
+      for (const auto &it : page->GetBitmap()) {
+        if (&it == first_shown) {
+          if (it.GetPosition() != first_shown_pos) {
+            wxPoint scroll = window_->GetViewStart();
+            scroll.x += it.GetX() - first_shown_pos.x;
+            scroll.y += it.GetY() - first_shown_pos.y;
+
+            window_->AdjustScrollbars();
+            window_->Scroll(scroll);
+            break;
+          }
+        }
+      }
+    }
+    window_->Refresh();
+  }
+  window_->SetBitmapPage(page_bitmap_ctrl_->GetBitmapPage());
+}
+
+bool Controller::IsOnEdge(const wxOrientation &orient, int pos) const {
+  int bottom_edge = window_->GetScrollRangeLimit(orient);
+  int scroll_pos = window_->GetScrollPos(orient);
+  bool is_top = scroll_pos == 0;
+
+  bool is_bottom = scroll_pos == bottom_edge;
+  is_bottom = is_bottom || window_->GetScrollRange(orient) <
+                               dimension::Get(window_->GetClientSize(), orient);
+
+  bool is_below_bottom = is_bottom && pos >= bottom_edge;
+  bool is_over_top = is_top && pos <= 0;
+  bool is_edge = (is_top && is_over_top) || (is_bottom && is_below_bottom);
+
+  return is_edge;
+}
+
+void Controller::OnLoadedImage(thread::LoadImageEvent &event) {
+  page_bitmap_ctrl_->AddBitmap(event.GetBitmap(),
+                               loader_->GetStreamPage(event.GetStream()),
+                               loader_->GetStreamPosInPage(event.GetStream()));
+  AdjustBitmap();
+  event.Skip();
+}
+
+void Controller::OnOpenedStreamFound(wxCommandEvent &event) {
+  page_bitmap_ctrl_->SetBitmapPage(loader_->GetOpenedPagePos());
+  AdjustBitmap();
+}
+
+int Controller::GetStep(wxDirection direction) {
+  int step = 0;
+
+  if (direction == wxUP || direction == wxLEFT) {
+    step = -1;
+  } else if (direction == wxDOWN || direction == wxRIGHT) {
+    step = 1;
+  }
+
+  if (dimension::GetOrient(direction) == wxHORIZONTAL && is_read_from_right_) {
+    step *= -1;
+  }
+
+  return step;
+}
+
+int Controller::GetStartX(wxDirection direction) {
+  auto vec_bitmap = page_bitmap_ctrl_->GetBitmap();
+
+  if (vec_bitmap.empty()) return 0;
+
+  if (direction == wxRIGHT && is_read_from_right_) {
+    direction = wxLEFT;
+  } else if (direction == wxLEFT && is_read_from_right_) {
+    direction = wxRIGHT;
+  }
+
+  if (direction == wxUP || direction == wxLEFT) {
+    auto bitmap = vec_bitmap.back();
+    if (bitmap) {
+      if (is_read_from_right_) {
+        if (bitmap->GetWidth() < window_->GetVirtualSize().GetWidth()) {
+          return (window_->GetVirtualSize().GetWidth() / 2 -
+                  bitmap->GetWidth() / 2) /
+                 2;
+        } else {
+          return window_->GetViewStart().x;
+        }
+      } else {
+        return bitmap->GetX() + bitmap->GetWidth() -
+               window_->GetClientSize().GetWidth();
+      }
+    }
+  } else if (direction == wxDOWN || direction == wxRIGHT) {
+    auto bitmap = vec_bitmap.at(0);
+    if (bitmap) {
+      if (is_read_from_right_) {
+        return bitmap->GetX() + bitmap->GetWidth() -
+               window_->GetClientSize().GetWidth();
+      } else {
+        if (bitmap->GetWidth() < window_->GetVirtualSize().GetWidth()) {
+          return (window_->GetVirtualSize().GetWidth() / 2 -
+                  bitmap->GetWidth() / 2) /
+                 2;
+        } else {
+          return window_->GetViewStart().x;
+        }
+      }
+    }
+  }
+
+  return 0;
+}
+int Controller::GetStartY(wxDirection direction) {
+  auto vec_bitmap = page_bitmap_ctrl_->GetBitmap();
+  if (vec_bitmap.empty()) return 0;
+
+  if (direction == wxRIGHT && is_read_from_right_) {
+    direction = wxLEFT;
+  } else if (direction == wxLEFT && is_read_from_right_) {
+    direction = wxRIGHT;
+  }
+
+  if (direction == wxUP || direction == wxLEFT) {
+    return window_->GetVirtualSize().GetHeight() -
+           window_->GetClientSize().GetHeight();
+  } else if (direction == wxDOWN || direction == wxRIGHT) {
+    return 0;
+  }
+
+  return 0;
+}
+
+wxPoint Controller::GetStartPosition(wxDirection direction) {
+  wxPoint pos = window_->GetViewStart();
+
+  return pos;
+}
+
+void Controller::ResetScroll(wxDirection direction) {
+  window_->Scroll(-1, GetStartY(direction));
+  window_->Scroll(GetStartX(direction), -1);
+}
+
+bool Controller::ChangePage(wxDirection direction) {
+  int step = GetStep(direction);
+
+  size_t idx = page_bitmap_ctrl_->GetPagePos() + step;
+
+  if (page_bitmap_ctrl_->IsPageExist(idx)) {
+    page_bitmap_ctrl_->SetBitmapPage(idx);
+    AdjustBitmap();
+    window_->AdjustScrollBar();
+    ResetScroll(direction);
+    return true;
+  }
+  return false;
+}
+
+bool Controller::ChangeFolder(wxDirection direction) {
+  int step = GetStep(direction);
+
+  AbstractHandler *handler = loader_->GetHandler();
+  if (handler && handler->GetParent()) {
+    handler->GetParent()->Traverse();
+    std::string path = handler->GetFromCurrent(step);
+    if (path != "") {
+      return Open(path);
+    }
+  }
+  return false;
+}
+
+bool Controller::Change(wxDirection direction) {
+  if (ChangePage(direction)) {
+    return true;
+  }
+
+  return ChangeFolder(direction);
+}
+
+void Controller::OnWindowScroll(wxScrollWinEvent &event) {
+  if (event.GetEventType() == wxEVT_SCROLLWIN_LINEUP ||
+      event.GetEventType() == wxEVT_SCROLLWIN_LINEDOWN) {
+    wxOrientation orient = dimension::GetOrient(event.GetOrientation());
+    if (IsOnEdge(orient, event.GetPosition())) {
+      wxDirection direction =
+          dimension::GetDirection(orient, event.GetPosition());
+      if (Change(direction)) return;
+    }
+  }
+  event.Skip();
+}
+
+void Controller::Clear() { loader_->Clear(); }
+
+}  // namespace reader
+
+}  // namespace fmr
