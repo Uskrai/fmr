daa2f004a124c44aa0161ba769e5254829e65dbb
Uskrai <30656271+Uskrai@users.noreply.github.com>
2021-01-28 16:47:42 +0800

    Create bitmap::PageBitmapCtrl

---
+++ include/fmr/bitmap/page_bitmap_ctrl.h
@@ -0,0 +1,68 @@
+/*
+ *  Copyright (c) 2021 Uskrai
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#ifndef FMR_BITMAP_PAGED_BITMAP_CTRL
+#define FMR_BITMAP_PAGED_BITMAP_CTRL
+
+#include "fmr/bitmap/bitmap_ctrl.h"
+#include "fmr/bitmap/bitmap_page.h"
+
+namespace fmr {
+
+namespace bitmap {
+
+std::vector<SBitmap *> BitmapPageToBitmapPtr(BitmapPage *page);
+
+class PageBitmapCtrl : public BitmapCtrl {
+  std::vector<BitmapPage> vec_page_;
+  size_t bitmap_per_page_ = -1;
+  size_t curr_page_ = -1;
+
+ public:
+  PageBitmapCtrl(PositionCtrl *pos_ctrl, Rescaler *rescaler,
+                 size_t bitmap_per_page = -1);
+
+  void SetBitmapPerPage(size_t size) { bitmap_per_page_ = size; };
+
+  void SetBitmapPage(size_t page);
+  BitmapPage *GetBitmapPage();
+
+  using BitmapCtrl::RecalcPosition;
+  void RecalcPosition(BitmapPage *page);
+
+  using BitmapCtrl::GetSize;
+  wxSize GetSize(BitmapPage *page);
+
+  std::vector<BitmapPage> &GetAllPage() { return vec_page_; }
+  const std::vector<BitmapPage> &GetAllPage() const { return vec_page_; }
+
+  size_t CountPage() const { return vec_page_.size(); }
+  size_t GetPagePos() const { return curr_page_; }
+
+  bool IsPageExist(size_t idx) { return idx < CountPage(); }
+
+  void AddBitmap(const SBitmap &bitmap, size_t page_pos, size_t bitmap_pos);
+
+ private:
+  void OnImageLoaded(thread::LoadImageEvent &event);
+};
+
+}  // namespace bitmap
+
+}  // namespace fmr
+
+#endif /* end of include guard: FMR_BITMAP_PAGED_BITMAP_CTRL */
---
+++ src/bitmap/page_bitmap_ctrl.cpp
@@ -0,0 +1,98 @@
+/*
+ *  Copyright (c) 2021 Uskrai
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#include "fmr/bitmap/page_bitmap_ctrl.h"
+
+namespace fmr {
+
+namespace bitmap {
+
+std::vector<SBitmap *> BitmapPageToBitmapPtr(BitmapPage *page) {
+  std::vector<SBitmap *> vec_ptr;
+
+  if (page) {
+    for (auto &it : page->GetBitmap()) {
+      vec_ptr.push_back(&it);
+    }
+  }
+  return vec_ptr;
+}
+
+PageBitmapCtrl::PageBitmapCtrl(PositionCtrl *pos_ctrl, Rescaler *rescaler,
+                               size_t bitmap_per_page)
+    : BitmapCtrl(pos_ctrl, rescaler) {
+  SetBitmapPerPage(bitmap_per_page);
+}
+
+void PageBitmapCtrl::AddBitmap(const SBitmap &bitmap, size_t page_pos,
+                               size_t bitmap_pos) {
+  if (page_pos == size_t(-1) || bitmap_pos == size_t(-1)) {
+    return;
+  }
+
+  while (vec_page_.size() <= page_pos) {
+    vec_page_.push_back(BitmapPage());
+  }
+
+  BitmapPage &page = vec_page_.at(page_pos);
+
+  while (page.Size() <= bitmap_pos) {
+    page.GetBitmap().push_back(SBitmap());
+  }
+
+  page.GetBitmap().at(bitmap_pos) = bitmap;
+
+  std::vector<SBitmap *> page_ptr;
+  for (auto &it : page.GetBitmap()) {
+    page_ptr.push_back(&it);
+  }
+  RecalcPosition(page_ptr);
+
+  if (page_pos == curr_page_) {
+    SetBitmapPage(curr_page_);
+  }
+}
+
+void PageBitmapCtrl::SetBitmapPage(size_t page) {
+  curr_page_ = page;
+
+  if (size_t(page) < vec_page_.size()) {
+    std::vector<SBitmap *> page_ptr;
+    for (auto &it : vec_page_.at(page).GetBitmap()) page_ptr.push_back(&it);
+    RecalcPosition(page_ptr);
+    SetBitmap(page_ptr);
+  }
+}
+
+BitmapPage *PageBitmapCtrl::GetBitmapPage() {
+  if (curr_page_ < vec_page_.size()) {
+    return &vec_page_.at(curr_page_);
+  }
+  return nullptr;
+}
+
+void PageBitmapCtrl::RecalcPosition(BitmapPage *page) {
+  return RecalcPosition(BitmapPageToBitmapPtr(page));
+}
+
+wxSize PageBitmapCtrl::GetSize(BitmapPage *page) {
+  return GetSize(BitmapPageToBitmapPtr(page));
+}
+
+}  // namespace bitmap
+
+}  // namespace fmr
