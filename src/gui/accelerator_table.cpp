9178269be9dad38d15d19b106aa57f033fea0cea
Muhammad Fathurrohim <30656271+Uskrai@users.noreply.github.com>
2021-02-18 23:20:11 +0800

    Create AcceleratorTable

---
+++ src/gui/accelerator_table.cpp
@@ -0,0 +1,39 @@
+/*
+ *  Copyright (c) 2021 Uskrai
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#include "fmr/gui/accelerator_table.h"
+
+#include "wx/menuitem.h"
+
+namespace fmr {
+
+namespace gui {
+
+void AcceleratorTable::AddEntry(int flags, int key_code, int command,
+                                wxMenuItem *item) {
+  wxAcceleratorEntry entry(flags, key_code, command, item);
+  if (item) item->SetAccel(&entry);
+  vec_entry_.push_back(entry);
+}
+
+wxAcceleratorTable AcceleratorTable::GetTable() const {
+  return wxAcceleratorTable(vec_entry_.size(), vec_entry_.data());
+}
+
+}  // namespace gui
+
+}  // namespace fmr
